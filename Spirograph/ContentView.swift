5c80fcf21467a8b5fbbde45b570faffebed3bc4a
keiren <kzaracho@roshka.com>
2020-11-11 21:27:04 -0300

    Initial Commit

---
+++ Spirograph/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Spirograph
+//
+//  Created by Karen Zaracho on 11/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
